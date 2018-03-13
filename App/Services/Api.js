81e0b7b38281d4109d8bf14adcdc3860b7572c66
Turfa Auliarachman <turfa_auliarachman@rocketmail.com>
2018-03-13 15:14:35 +0700

    add API for user

--- App/Services/Api.js
+++ App/Services/Api.js
@@ -2,7 +2,10 @@
 import apisauce from 'apisauce'
 import config from '../Config/AppConfig'
 
-// our "constructor"
+const addAuthorizationHeader = (userToken) => ({
+  headers: {Authorization: `JWT ${userToken}`}
+})
+
 const create = (baseURL = config.baseURL) => {
   const api = apisauce.create({
     baseURL,
@@ -15,8 +18,26 @@ const create = (baseURL = config.baseURL) => {
   const login = (username, password) =>
     api.post('users/login', {username, password})
 
+  const getMe = (userToken) =>
+    api.get('users/me', {}, addAuthorizationHeader(userToken))
+
+  const getUsers = (userToken) =>
+    api.get('users', {}, addAuthorizationHeader(userToken))
+
+  const postUser = (userToken, name, username, password, divisions, roles) =>
+    api.post('users',
+             {name, username, password, divisions, roles},
+             addAuthorizationHeader(userToken))
+
+  const updateUser = (userToken, id, userData) =>
+    api.put('users', {...userData, id}, addAuthorizationHeader(userToken))
+
   return {
-    login
+    login,
+    getMe,
+    getUsers,
+    postUser,
+    updateUser
   }
 }
 
