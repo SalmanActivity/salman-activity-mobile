982121e582cefa98e4e23f3bfd3ef2df4d7560e2
mahdiarn <diarn_7@live.com>
2018-03-16 10:18:15 +0700

    Add LocationSagas.js

---
+++ App/Sagas/LocationSagas.js
@@ -0,0 +1,52 @@
+import { call, put } from 'redux-saga/effects'
+import LocationActions from '../Redux/LocationRedux'
+
+export function * getMe (api, action) {
+  const {locatinoToken} = action
+  const response = yield call(api.getMe, locationToken)
+
+  if (response.ok) {
+    yield put(LocationActions.getMeSuccess(response.data))
+  } else {
+    const {cause} = response.data
+    yield put(LocationActions.getMeFailure(cause))
+  }
+}
+
+export function * getLocations (api, action) {
+  const {locationToken} = action
+  const response = yield call(api.getLocations, locationToken)
+
+  if (response.ok) {
+    yield put(LocationActions.getLocationsSuccess(response.data))
+  } else {
+    const {cause} = response.data
+    yield put(LocationActions.getLocationsFailure(cause))
+  }
+}
+
+export function * newLocation (api, action) {
+  const {locationToken, name} = action
+  const response = yield call(api.postLocation,
+                              locationToken,
+                              name)
+
+  if (response.ok) {
+    yield put(LocationActions.postLocationSuccess())
+  } else {
+    const {cause} = response.data
+    yield put(LocationActions.postLocationFailure(cause))
+  }
+}
+
+export function * updateLocation (api, action) {
+  const {locationToken, id, locationData} = action
+  const response = yield call(api.updateLocation, locationToken, id, locationData)
+
+  if (response.ok) {
+    yield put(LocationActions.updateLocationSuccess())
+  } else {
+    const {cause} = response.data
+    yield put(LocationActions.updateLocationFailure(cause))
+  }
+}
