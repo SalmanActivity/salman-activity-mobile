616f755301dcef56d7974d6446764d213adca60d
Turfa Auliarachman <turfa_auliarachman@rocketmail.com>
2018-03-30 23:08:06 +0700

    add request redux

--- App/Redux/RequestRedux.js
+++ App/Redux/RequestRedux.js
@@ -4,7 +4,7 @@ import Immutable from 'seamless-immutable'
 /* ------------- Types and Action Creators ------------- */
 
 const { Types, Creators } = createActions({
-  getRequests: ['userToken'],
+  getRequests: ['userToken', 'month', 'year'],
   getRequestsSuccess: ['requests'],
   getRequestsFailure: ['error'],
 
