e4a0d645ca2ee988f9dd88315960084f99274e6c
Turfa Auliarachman <turfa_auliarachman@rocketmail.com>
2018-04-11 21:24:21 +0700

    add getRequest saga

--- App/Sagas/RequestSagas.js
+++ App/Sagas/RequestSagas.js
@@ -20,6 +20,25 @@ export function * getRequests (api, action) {
   }
 }
 
+export function * getRequests (api, action) {
+  const {userToken, id} = action
+  const response = yield call(api.getRequest, userToken, id)
+
+  if (response.ok) {
+    yield put(RequestActions.getRequestSuccess(response.data))
+  } else {
+    let cause
+
+    if (response.data) {
+      cause = response.data.error.cause
+    } else {
+      cause = 'Connection Error'
+    }
+
+    yield put(RequestActions.getRequestFailure(cause))
+  }
+}
+
 export function * newRequest (api, action) {
   const {userToken, name, description, division, location,
     startTime, endTime, participantNumber, participantDescription,
