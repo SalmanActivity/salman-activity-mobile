f9eac4b189a6d18a861b6338b150736b7e5ccef8
Mahdiar Naufal <mahdiarnaufal@gmail.com>
2018-04-13 00:05:38 +0700

    Add buttonUpload and buttonPost style

--- App/Forms/Styles/ActivityReportFormStyles.js
+++ App/Forms/Styles/ActivityReportFormStyles.js
@@ -2,7 +2,11 @@ import {StyleSheet} from 'react-native'
 import {Colors, Metrics} from '../../Themes'
 
 export default StyleSheet.create({
-  button: {
+  buttonUpload: {
+    marginTop: Metrics.baseMargin,
+    backgroundColor: Colors.facebook
+  },
+  buttonPost: {
     marginTop: Metrics.baseMargin,
     backgroundColor: Colors.tealPrimary
   }
