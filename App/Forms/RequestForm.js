8e42a4242cc7c6684e53ef247cdf5eef9e866508
Turfa Auliarachman <turfa_auliarachman@rocketmail.com>
2018-04-14 14:32:57 +0700

    use Picker for Division and Location on RequestForm

--- App/Forms/RequestForm.js
+++ App/Forms/RequestForm.js
@@ -3,6 +3,7 @@ import {ScrollView} from 'react-native'
 import {Field, reduxForm} from 'redux-form/immutable'
 import {Button} from 'react-native-elements'
 import TextInput from './TextInputWrapper'
+import Picker from './PickerWrapper'
 
 import styles from './Styles/FormStyles'
 
@@ -19,15 +20,21 @@ const RequestForm = (props) => (
       component={TextInput}
       placeholder='Deskripsi Kegiatan'
     />
-    <Field
-      name='division'
-      component={TextInput}
-      placeholder='Bidang'
-    />
+    {props.admin && (
+      <Field
+        name='division'
+        component={Picker}
+        placeholder='Bidang'
+        data={props.divisions}
+        value={props.division}
+      />
+    )}
     <Field
       name='location'
-      component={TextInput}
+      component={Picker}
       placeholder='Lokasi'
+      data={props.locations}
+      value={props.location}
     />
     <Field
       name='startTime'
