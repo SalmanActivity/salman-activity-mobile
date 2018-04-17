ab997ad2f90907affd4e12b02a2c1e5ac1c6ea22
Mahdiar Naufal <mahdiarnaufal@gmail.com>
2018-04-17 14:54:07 +0700

    add ReportSagas.js

---
+++ App/Sagas/ReportSagas.js
@@ -0,0 +1,61 @@
+import { call, put } from 'redux-saga/effects'
+import LocationActions from '../Redux/LocationRedux'
+
+export function * getLocations (api, action) {
+  const {userToken} = action
+  const response = yield call(api.getLocations, userToken)
+
+  if (response.ok) {
+    yield put(LocationActions.getLocationsSuccess(response.data))
+  } else {
+    let cause
+
+    if (response.data) {
+      cause = response.data.error.cause
+    } else {
+      cause = 'Connection Error'
+    }
+
+    yield put(LocationActions.getLocationsFailure(cause))
+  }
+}
+
+export function * newLocation (api, action) {
+  const {userToken, name} = action
+  const response = yield call(api.postLocation,
+                              userToken,
+                              name)
+
+  if (response.ok) {
+    yield put(LocationActions.postLocationSuccess())
+  } else {
+    let cause
+
+    if (response.data) {
+      cause = response.data.error.cause
+    } else {
+      cause = 'Connection Error'
+    }
+
+    yield put(LocationActions.postLocationFailure(cause))
+  }
+}
+
+export function * updateLocation (api, action) {
+  const {userToken, id, locationData} = action
+  const response = yield call(api.updateLocation, userToken, id, locationData)
+
+  if (response.ok) {
+    yield put(LocationActions.updateLocationSuccess())
+  } else {
+    let cause
+
+    if (response.data) {
+      cause = response.data.error.cause
+    } else {
+      cause = 'Connection Error'
+    }
+
+    yield put(LocationActions.updateLocationFailure(cause))
+  }
+}
